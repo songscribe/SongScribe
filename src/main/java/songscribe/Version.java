86469975f0cce7dc1ce4e5015a0ccd69cf7442f0
himadri77 <ipwd01>
2013-09-23 12:47:39 +0200

    Developing maven dependencies.

--- src/main/java/songscribe/Version.java
+++
@@ -1,7 +0,0 @@
-// This is an auto generated code. DO NOT MODIFY!
-package songscribe;
-public final class Version {
-   public static final String MAJOR_VERSION="1";
-   public static final String MINOR_VERSION="17";
-   public static final String BUILD_VERSION="20130919125611";
-}
