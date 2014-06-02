8b620f5390944fd91996af29dd7cc240df19385a
himadri77 <ipwd01>
2014-06-02 14:27:42 +0200

    Lilypond

--- src/main/java/songscribe/ui/mainframeactions/ExportLilypondAnnotationAction.java
+++ src/main/java/songscribe/ui/mainframeactions/ExportLilypondAnnotationAction.java
@@ -1,4 +1,4 @@
-/* 
+/*
 SongScribe song notation program
 Copyright (C) 2006-2007 Csaba Kavai
 
@@ -22,95 +22,451 @@ Created on Aug 6, 2006
 package songscribe.ui.mainframeactions;
 
 import org.apache.log4j.Logger;
-import songscribe.data.*;
-import songscribe.music.*;
-import songscribe.ui.Constants;
+
+import songscribe.data.Interval;
+import songscribe.data.MyAcceptFilter;
+import songscribe.data.PlatformFileDialog;
+import songscribe.data.TupletIntervalData;
+
+import songscribe.music.Composition;
+import songscribe.music.GraceSemiQuaver;
+import songscribe.music.KeyType;
+import songscribe.music.Line;
+import songscribe.music.Note;
+import songscribe.music.NoteType;
+import songscribe.music.Tempo;
+
 import songscribe.ui.MainFrame;
 import songscribe.ui.Utilities;
 
-import javax.swing.*;
 import java.awt.event.ActionEvent;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.AbstractMap;
-import java.util.HashMap;
-import java.util.Map;
 
-/** The following features are not supported in abc 2.1
- * 1. Accidentals in parenthesis. I opened a forum for this topic (with no answer yet): http://abcnotation.com/forums/viewtopic.php?f=7&t=260
- * 2. Beat change.
- * 3. Glissandos. I substituted them with slurs for now.
- * 4. On syllabified lyrics no long hyphen between compound words (like God-Realisation)
- * 5. Syllables under grace notes. Solution: put together with the syllable of next note with \
- * 6. Forcing syllable under a rest (new SongScribe feature)
- * @author Csaba Kávai
- */
-public class ExportLilypondAnnotationAction extends AbstractAction {
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JOptionPane;
+
+public class ExportLilypondAnnotationAction extends AbstractAction
+{
+
+    // left http://www.lilypond.org/doc/v2.18/Documentation/learning-big-page#fundamental-concepts
+    private static final String[] PITCH_TYPES = { "c", "des", "d", "ees", "e", "f", "ges", "g", "aes", "a", "bes", "b" };
     private Logger logger = Logger.getLogger(ExportLilypondAnnotationAction.class);
-    private PlatformFileDialog pfd;
     private MainFrame mainFrame;
+    private PlatformFileDialog pfd;
 
-    public ExportLilypondAnnotationAction(MainFrame mainFrame) {
+    public ExportLilypondAnnotationAction(MainFrame mainFrame)
+    {
         this.mainFrame = mainFrame;
         putValue(Action.NAME, "Export as LilyPond Notation...");
         pfd = new PlatformFileDialog(mainFrame, "Export as LilyPond Notation", false, new MyAcceptFilter("LilyPond Files", "ly"));
     }
 
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent e)
+    {
         pfd.setFile(Utilities.getSongTitleFileNameForFileChooser(mainFrame.getMusicSheet()));
-        if(pfd.showDialog()){
+        if (pfd.showDialog())
+        {
             File saveFile = pfd.getFile();
-            if(!saveFile.getName().toLowerCase().endsWith(".ly")){
-                saveFile = new File(saveFile.getAbsolutePath()+".ly");
+
+            if (!saveFile.getName().toLowerCase().endsWith(".ly"))
+            {
+                saveFile = new File(saveFile.getAbsolutePath() + ".ly");
             }
-            if(saveFile.exists()){
-                int answ = JOptionPane.showConfirmDialog(mainFrame, "The file "+saveFile.getName()+" already exists. Do you want to overwrite it?",
-                        mainFrame.PROGNAME, JOptionPane.YES_NO_OPTION);
-                if(answ==JOptionPane.NO_OPTION){
+
+            if (saveFile.exists())
+            {
+                int answ = JOptionPane.showConfirmDialog(mainFrame,
+                        "The file " + saveFile.getName() + " already exists. Do you want to overwrite it?", mainFrame.PROGNAME,
+                        JOptionPane.YES_NO_OPTION);
+
+                if (answ == JOptionPane.NO_OPTION)
+                {
                     return;
                 }
             }
+
             PrintWriter writer = null;
-            try {
+
+            try
+            {
                 writer = new PrintWriter(saveFile);
-                writeABC(writer);
-            } catch (IOException e1) {
+                writeLilypond(writer);
+            }
+            catch (IOException e1)
+            {
                 mainFrame.showErrorMessage(MainFrame.COULDNOTSAVEMESSAGE);
                 logger.error("Saving LilyPond", e1);
-            } finally {
-                if (writer != null) {
+            }
+            finally
+            {
+                if (writer != null)
+                {
                     writer.close();
                 }
             }
         }
     }
-    
-    public void writeABC(PrintWriter writer) {
+
+    public void writeLilypond(PrintWriter writer)
+    {
         Composition composition = mainFrame.getMusicSheet().getComposition();
-        writer.println("\\score {");
+
+        writer.println("\\relative c' {");
+        writer.println("\\autoBeamOff");
+        writer.println("\\clef \"treble\" ");
+        writer.print(translateTempo(composition.getTempo()));
+        writer.println();
         writer.println("}");
-
-    }
-
-
-    String translatePitch(int yPos) {
-        StringBuilder sb = new StringBuilder();
-        char letter = (char)((getPitchType(yPos) + 1 ) % 7 + 'a');
-        sb.append(letter);
-        for (int y = yPos;y < 7; y+=7) {
-            sb.append('\'');
-        }
-        return sb.toString();
     }
 
     /**
      * @return 0 for B, 1 for C, 2 for D, ..., 6 for A
      */
-    int getPitchType(int yPos){
-        return yPos<=0 ? -yPos % 7 : (7 - yPos % 7) % 7;
+    int getPitchType(int yPos)
+    {
+        return (yPos <= 0) ? (-yPos % 7) : ((7 - (yPos % 7)) % 7);
     }
 
+    String translateArticulation(Note note)
+    {
+        if (note.getDurationArticulation() != null)
+        {
+            switch (note.getDurationArticulation())
+            {
 
+                case STACCATO:
+                    return "-.";
 
+                case TENUTO:
+                    return "--";
+
+                default:
+                    throw new RuntimeException("New articulation introduced without implementing lilypond conversion");
+            }
+        }
+
+        if (note.getForceArticulation() != null)
+        {
+            switch (note.getForceArticulation())
+            {
+
+                case ACCENT:
+                    return "->";
+
+                default:
+                    throw new RuntimeException("New articulation introduced without implementing lilypond conversion");
+            }
+        }
+
+        return "";
+    }
+
+    String translateBeams(Note note)
+    {
+        int noteIndex = note.getLine().getNoteIndex(note);
+
+        if (note.getLine().getBeamings().isStartOfAnyInterval(noteIndex))
+        {
+            return "[";
+        }
+
+        if (note.getLine().getBeamings().isEndOfAnyInterval(noteIndex))
+        {
+            return "]";
+        }
+
+        return "";
+    }
+
+    String translateDotted(Note note)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < note.getDotted(); i++)
+        {
+            sb.append('.');
+        }
+
+        return sb.toString();
+    }
+
+    String translateDuration(Note note)
+    {
+        if (note.getNoteType() == NoteType.GRACEQUAVER)
+        {
+            return "8";
+        }
+
+        if (note.getNoteType() == NoteType.GRACESEMIQUAVER)
+        {
+            return "16";
+        }
+
+        if (note.getDefaultDuration() > 0)
+        {
+            return Integer.toString(Composition.PPQ * 4 / note.getDefaultDuration());
+        }
+        else
+        {
+            return "";
+        }
+    }
+
+    String translateDynamics(Note note)
+    {
+        int noteIndex = note.getLine().getNoteIndex(note);
+
+        if (note.getLine().getCrescendo().isEndOfAnyInterval(noteIndex) || note.getLine().getDiminuendo().isEndOfAnyInterval(noteIndex))
+        {
+            return "\\!";
+        }
+
+        if (note.getLine().getCrescendo().isStartOfAnyInterval(noteIndex))
+        {
+            return "\\<";
+        }
+
+        if (note.getLine().getDiminuendo().isStartOfAnyInterval(noteIndex))
+        {
+            return "\\>";
+        }
+
+        return "";
+    }
+
+    String translateImlicitRepeatLeftAtLineBeginning(Line line)
+    {
+        NoteType firstRepeatSign = null;
+
+        for (int n = 0; (n < line.noteCount()) && (firstRepeatSign == null); n++)
+        {
+            if (line.getNote(n).getNoteType().isRepeat())
+            {
+                firstRepeatSign = line.getNote(n).getNoteType();
+            }
+        }
+
+        if ((firstRepeatSign == NoteType.REPEATRIGHT) || (firstRepeatSign == NoteType.REPEATLEFTRIGHT))
+        {
+            return translateRepeatAndBarLine(NoteType.REPEATLEFT);
+        }
+        else
+        {
+            return "";
+        }
+    }
+
+    String translateImlicitRepeatRightAtLineEnd(Line line)
+    {
+        NoteType lastRepeatSign = null;
+
+        for (int n = line.noteCount() - 1; (n >= 0) && (lastRepeatSign == null); n--)
+        {
+            if (line.getNote(n).getNoteType().isRepeat())
+            {
+                lastRepeatSign = line.getNote(n).getNoteType();
+            }
+        }
+
+        if ((lastRepeatSign == NoteType.REPEATLEFT) || (lastRepeatSign == NoteType.REPEATLEFTRIGHT))
+        {
+            return translateRepeatAndBarLine(NoteType.REPEATRIGHT);
+        }
+        else
+        {
+            return "";
+        }
+    }
+
+    String translateKey(KeyType keyType, int number)
+    {
+        String[] sharpKeys = { "c", "g", "d", "a", "e", "b", "fis", "cis" };
+        String[] flatKeys = { "c", "f", "bes", "ees", "aes", "des", "ges", "ces" };
+        String key;
+
+        if (keyType == KeyType.SHARPS)
+        {
+            key = sharpKeys[number];
+        }
+        else
+        {
+            key = flatKeys[number];
+        }
+
+        return "\\key" + key + " \\major";
+    }
+
+    String translateLine(Line line)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(translateKey(line.getKeyType(), line.getKeys()));
+        sb.append(translateImlicitRepeatLeftAtLineBeginning(line));
+        sb.append(translateImlicitRepeatRightAtLineEnd(line));
+
+        return sb.toString();
+    }
+
+    String translateNote(Note note)
+    {
+        StringBuilder sb = new StringBuilder();
+        NoteType noteType = note.getNoteType();
+
+        if (noteType.isGraceNote())
+        {
+            sb.append("\\acciaccatura ");
+        }
+
+        if (noteType == NoteType.GRACESEMIQUAVER)
+        {
+            sb.append("{");
+            int origY = note.getYPos();
+
+            note.setYPos(((GraceSemiQuaver) note).getY0Pos());
+            sb.append(translatePitch(note));
+            note.setYPos(origY);
+            sb.append(translateDuration(note));
+            sb.append(' ');
+        }
+
+        if (noteType.isNote())
+        {
+            sb.append(translatePitch(note));
+        }
+
+        if (noteType.isRest())
+        {
+            sb.append("r");
+        }
+
+        sb.append(translateDuration(note));
+        sb.append(translateDotted(note));
+        sb.append(translateTie(note));
+        sb.append(translateSlur(note));
+        sb.append(translateArticulation(note));
+        sb.append(translateDynamics(note));
+        sb.append(translateBeams(note));
+        sb.append(translateText(note));
+        sb.append(translateTuplet(note));
+        if (noteType == NoteType.GRACESEMIQUAVER)
+        {
+            sb.append("}");
+        }
+
+        return sb.toString();
+    }
+
+    String translatePitch(Note note)
+    {
+        int pitch = note.getPitch() - 60;
+        StringBuilder sb = new StringBuilder(PITCH_TYPES[pitch % 12]);
+        int type = pitch / 12;
+
+        if (pitch < 0)
+        {
+            type--;
+        }
+
+        for (int i = 0; i < type; i++)
+        {
+            sb.insert(1, (pitch < 0) ? ',' : '\'');
+        }
+
+        return sb.toString();
+    }
+
+    String translateRepeatAndBarLine(NoteType noteType)
+    {
+        switch (noteType)
+        {
+
+            case REPEATLEFT:
+                return "\\repeat volta 2 {";
+
+            case REPEATRIGHT:
+                return "}";
+
+            case REPEATLEFTRIGHT:
+                return "} \\repeat volta 2 {";
+
+            case SINGLEBARLINE:
+                return "\\bar \"|\"";
+
+            case DOUBLEBARLINE:
+                return "\\bar \"||\"";
+
+            case FINALDOUBLEBARLINE:
+                return "\\bar \"|.\"";
+
+            default:
+                return "";
+        }
+    }
+
+    String translateSlur(Note note)
+    {
+        int noteIndex = note.getLine().getNoteIndex(note);
+
+        if (note.getLine().getSlurs().isStartOfAnyInterval(noteIndex))
+        {
+            return "(";
+        }
+
+        if (note.getLine().getSlurs().isEndOfAnyInterval(noteIndex))
+        {
+            return ")";
+        }
+
+        return "";
+    }
+
+    String translateTempo(Tempo tempo)
+    {
+        return "\\tempo \"" + tempo.getTempoDescription() + "\" " + translateDuration(tempo.getTempoType().getNote()) + " = "
+              + tempo.getVisibleTempo();
+    }
+
+    String translateText(Note note)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        if (note.getAnnotation() != null)
+        {
+
+            // TODO text alignments
+            sb.append((note.getAnnotation().getyPos() < 0) ? "^" : "_").append("\\markup { ").append(note.getAnnotation().getAnnotation())
+              .append("}");
+        }
+
+        return sb.toString();
+    }
+
+    String translateTie(Note note)
+    {
+        return note.getLine().getTies().isStartOfAnyInterval(note.getLine().getNoteIndex(note)) ? "~" : "";
+    }
+
+    String translateTuplet(Note note)
+    {
+        int noteIndex = note.getLine().getNoteIndex(note);
+
+        if (note.getLine().getTuplets().isStartOfAnyInterval(noteIndex))
+        {
+            Interval interval = note.getLine().getTuplets().findInterval(noteIndex);
+            int grade = TupletIntervalData.getGrade(interval);
+
+            return "\\tuplet " + grade + "/" + (grade - 1) + " {";
+        }
+
+        if (note.getLine().getSlurs().isEndOfAnyInterval(noteIndex))
+        {
+            return "}";
+        }
+
+        return "";
+    }
 }
--- src/test/java/songscribe/ui/mainframeactions/ExportLilypondAnnotationActionTest.java
+++ src/test/java/songscribe/ui/mainframeactions/ExportLilypondAnnotationActionTest.java
@@ -1,32 +1,88 @@
 package songscribe.ui.mainframeactions;
 
+import static junit.framework.Assert.assertEquals;
+
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Before;
 import org.junit.Test;
-import songscribe.music.Crotchet;
 
-import static junit.framework.Assert.assertEquals;
+import songscribe.music.BreathMark;
+import songscribe.music.Crotchet;
+import songscribe.music.CrotchetRest;
+import songscribe.music.Demisemiquaver;
+import songscribe.music.DemisemiquaverRest;
+import songscribe.music.DoubleBarLine;
+import songscribe.music.DurationArticulation;
+import songscribe.music.FinalDoubleBarLine;
+import songscribe.music.ForceArticulation;
+import songscribe.music.GraceQuaver;
+import songscribe.music.GraceSemiQuaver;
+import songscribe.music.Minim;
+import songscribe.music.MinimRest;
+import songscribe.music.Note;
+import songscribe.music.Quaver;
+import songscribe.music.QuaverRest;
+import songscribe.music.RepeatLeft;
+import songscribe.music.RepeatLeftRight;
+import songscribe.music.RepeatRight;
+import songscribe.music.Semibreve;
+import songscribe.music.SemibreveRest;
+import songscribe.music.Semiquaver;
+import songscribe.music.SemiquaverRest;
+import songscribe.music.SingleBarLine;
 
 /**
  * Created by Kávai on 2014.05.03..
  */
-public class ExportLilypondAnnotationActionTest {
+public class ExportLilypondAnnotationActionTest
+{
     private ExportLilypondAnnotationAction action;
 
-    @Before
-    public void setUp() throws Exception {
+    @Before public void setUp() throws Exception
+    {
         action = new ExportLilypondAnnotationAction(null);
-
     }
 
-    @Test
-    public void testTranslatePitch() {
-        assertEquals("d'''", action.translatePitch(-9));
-        assertEquals("c'''", action.translatePitch(-8));
-        assertEquals("b''", action.translatePitch(-7));
-        assertEquals("c''", action.translatePitch(-1));
-        assertEquals("b'", action.translatePitch(0));
-        assertEquals("c'", action.translatePitch(6));
-        assertEquals("b", action.translatePitch(7));
-        assertEquals("e", action.translatePitch(11));
+    @Test public void testTranslateArticulation() throws Exception
+    {
+        Note note = new Crotchet();
+
+        for (DurationArticulation durationArticulation : DurationArticulation.values())
+        {
+            note.setDurationArticulation(durationArticulation);
+            assertTrue(action.translateArticulation(note).startsWith("-"));
+        }
+
+        for (ForceArticulation forceArticulation : ForceArticulation.values())
+        {
+            note.setForceArticulation(forceArticulation);
+            assertTrue(action.translateArticulation(note).startsWith("-"));
+        }
+    }
+
+    @Test public void testTranslateDuration()
+    {
+        assertEquals("1", action.translateDuration(new Semibreve()));
+        assertEquals("2", action.translateDuration(new Minim()));
+        assertEquals("4", action.translateDuration(new Crotchet()));
+        assertEquals("8", action.translateDuration(new Quaver()));
+        assertEquals("16", action.translateDuration(new Semiquaver()));
+        assertEquals("32", action.translateDuration(new Demisemiquaver()));
+        assertEquals("1", action.translateDuration(new SemibreveRest()));
+        assertEquals("2", action.translateDuration(new MinimRest()));
+        assertEquals("4", action.translateDuration(new CrotchetRest()));
+        assertEquals("8", action.translateDuration(new QuaverRest()));
+        assertEquals("16", action.translateDuration(new SemiquaverRest()));
+        assertEquals("32", action.translateDuration(new DemisemiquaverRest()));
+        assertEquals("", action.translateDuration(new GraceQuaver()));
+        assertEquals("", action.translateDuration(new GraceSemiQuaver()));
+        assertEquals("", action.translateDuration(new RepeatLeft()));
+        assertEquals("", action.translateDuration(new RepeatRight()));
+        assertEquals("", action.translateDuration(new RepeatLeftRight()));
+        assertEquals("", action.translateDuration(new BreathMark()));
+        assertEquals("", action.translateDuration(new SingleBarLine()));
+        assertEquals("", action.translateDuration(new DoubleBarLine()));
+        assertEquals("", action.translateDuration(new FinalDoubleBarLine()));
     }
 }
