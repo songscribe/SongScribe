c771d5145529473c29691f1c6d5abdc880759612
Aparajita Fishman <aparajita@aparajita.com>
2014-10-20 09:01:54 -0400

    Make sure drawing is antialiased

--- src/main/java/songscribe/ui/MusicSheet.java
+++ src/main/java/songscribe/ui/MusicSheet.java
@@ -290,6 +290,8 @@ public final class MusicSheet extends JComponent implements MouseListener, Mouse
         }
 
         Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
 
         if (repaintImage) {
             Graphics2D g2MsImage = msImage.createGraphics();
